12387dc90bd78d2e599983b5bf6d2df32b30cbcc
anaisrandria <anais.randriamitantsoa@gmail.com>
2024-10-10 17:19:42 +0200

    fix: link to hashtag page by tag_id

--- Niveau1/tags.php
+++ Niveau1/tags.php
@@ -70,7 +70,8 @@
                     tags.id as tag_id,  
                     posts.user_id, 
                     count(likes.id) as like_number,  
-                    GROUP_CONCAT(DISTINCT tags.label) AS taglist 
+                    GROUP_CONCAT(DISTINCT tags.label) AS taglist,
+                    GROUP_CONCAT(DISTINCT tags.id) AS tag_idlist
                     FROM posts_tags as filter 
                     JOIN posts ON posts.id=filter.post_id
                     JOIN users ON users.id=posts.user_id
@@ -78,6 +79,7 @@
                     LEFT JOIN tags       ON posts_tags.tag_id  = tags.id 
                     LEFT JOIN likes      ON likes.post_id  = posts.id 
                     WHERE filter.tag_id = '$tagId' 
+                    
                     GROUP BY posts.id
                     ORDER BY posts.created DESC  
                     ";
--- Niveau1/wall.php
+++ Niveau1/wall.php
@@ -53,8 +53,9 @@
                  * Etape 3: récupérer tous les messages de l'utilisatrice
                  */
                 $laQuestionEnSql = "
-                    SELECT posts.content, posts.created, users.alias as author_name, posts.user_id, tags.id as tag_id,
-                    COUNT(likes.id) as like_number, GROUP_CONCAT(DISTINCT tags.label) AS taglist 
+                    SELECT posts.content, posts.created, users.alias as author_name, posts.user_id,
+                    COUNT(likes.id) as like_number, GROUP_CONCAT(DISTINCT tags.label) AS taglist,
+                    GROUP_CONCAT(DISTINCT tags.id) AS tag_idlist
                     FROM posts
                     JOIN users ON  users.id=posts.user_id
                     LEFT JOIN posts_tags ON posts.id = posts_tags.post_id  
