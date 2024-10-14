a69b1e8694d0b2b768ccbc9b924ed26fae95631a
anaisrandria <anais.randriamitantsoa@gmail.com>
2024-10-14 15:54:45 +0200

    add: logout configuration

--- Niveau1/header.php
+++ Niveau1/header.php
@@ -5,10 +5,14 @@
 <header>
     <a href='admin.php'><img src="resoc.jpg" alt="Logo de notre réseau social" /></a>
     <nav id="menu">
-        <a href="news.php">Actualités</a>
-        <a href="wall.php?user_id=<?php echo $_SESSION['connected_id'] ?>">Mur</a>
-        <a href="feed.php?user_id=<?php echo $_SESSION['connected_id'] ?>">Flux</a>
-        <a href="tags.php?tag_id=1">Mots-clés</a>
+        
+        <?php
+        if (isset($_SESSION['connected_id'])) { ?>
+            <a href="news.php">Actualités</a>
+            <a href="wall.php?user_id=<?php echo $_SESSION['connected_id'] ?>">Mur</a>
+            <a href="feed.php?user_id=<?php echo $_SESSION['connected_id'] ?>">Flux</a>
+            <a href="tags.php?tag_id=1">Mots-clés</a>
+        <?php } ?>
     </nav>
     <nav id="user">
         <?php 
@@ -22,6 +26,7 @@
             <li><a href="settings.php?user_id=<?php echo $_SESSION['connected_id'] ?>">Paramètres</a></li>
             <li><a href="followers.php?user_id=<?php echo $_SESSION['connected_id'] ?>">Mes suiveurs</a></li>
             <li><a href="subscriptions.php?user_id=<?php echo $_SESSION['connected_id'] ?>">Mes abonnements</a></li>
+            <li><a href="logout.php">Se déconnecter</a></li>
         </ul>
         <?php } ?>
     </nav>
